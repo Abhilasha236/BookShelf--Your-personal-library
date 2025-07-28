5d94c43bc7d7d6b2447d370beff344c28e5b2aee
Abhilasha <abhilasha23062003@gmail.com>
2025-07-28 21:39:27 +0530

    changes mads

--- server/serverback.js
+++ server/serverback.js
@@ -15,10 +15,7 @@ app.use('/api/books', bookRoutes);
 app.use('/api', authRoutes);      // /api/login and /api/signup will work now
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
